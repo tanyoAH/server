2a12e543cdd8875b4395133579a87f2a443058a6
Sasha Varlamov <sasha@sashavarlamov.com>
2017-06-04 09:00:37 +0800

    Fix bson field for id in activity response

--- models/basic_activity_response.go
+++ models/basic_activity_response.go
@@ -3,7 +3,7 @@ package models
 import "gopkg.in/mgo.v2/bson"
 
 type BasicActivityResponse struct {
-	Id           bson.ObjectId `json:"id" bson:"id"`
+	Id           bson.ObjectId `json:"id" bson:"_id"`
 	Name         string        `json:"name" bson:"name"`
 	Price        float64       `json:"price" bson:"price"`
 	ThumbnailUrl string        `json:"thumbnailUrl" bson:"thumbnail_url"`
